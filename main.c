b2eb075732b03b3498b8e9d72f58611532fb508c
Fabri0607UNA <fabricio.alfaro.arce@est.una.ac.cr>
2024-09-30 10:52:54 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Inicialización del programa
+    printf("Hola, mundo!\n");
+
+    // Código principal
+
+    // Finalización del programa
+    return 0;
+}
